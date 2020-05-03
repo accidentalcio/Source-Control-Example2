b3c65c0db84dfcdd883330f8f54e3b6594cd5daa
Timothy Chester <timothy.chester@outlook.com>
2020-05-03 19:47:08 -0400

    Initial Commit

---
+++ Source Control Example2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control Example2
+//
+//  Created by Timothy Chester on 5/3/20.
+//  Copyright © 2020 Timothy Chester. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
