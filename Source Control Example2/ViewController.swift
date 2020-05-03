ad1f88d4343ca64e61070a5698b7ac6654118674
Timothy Chester <timothy.chester@outlook.com>
2020-05-03 19:51:11 -0400

    Here's a commit message.

--- Source Control Example2/ViewController.swift
+++ Source Control Example2/ViewController.swift
@@ -22,6 +22,17 @@ class ViewController: NSViewController {
         }
     }
 
+    /**
+     Adds two numbers together and returns
+     the result
+     
+     - parameter num1: The first number.
+     - parameter num2: The second number
+     - returns: The sum of num1 and num2
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
 
 }
 
